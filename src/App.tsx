a6fcbc57990da19aadcc7792aea25a4eb852f5b0
Chiara Gandolfi <Chiara.Gandolfi@softwire.com>
2022-11-16 11:03:52 +0000

    implemented button with counter component

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 import NASA from "./components/NASA";
 import TitleParagraphImageComponent from "./components/TitleParagraphImageComponent";
+import ButtonWithCounter from "./components/ButtonWithCounter";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-          <TitleParagraphImageComponent title={"Just a cat"} p1={"paragraph1"} p2={"paragraph2"} image={"http://placekitten.com/200/300"}></TitleParagraphImageComponent>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      <NASA></NASA>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <ButtonWithCounter></ButtonWithCounter>
+                <TitleParagraphImageComponent title={"Just a cat"} p1={"paragraph1"} p2={"paragraph2"}
+                                              image={"http://placekitten.com/200/300"}></TitleParagraphImageComponent>
+
+                <NASA></NASA>
+            </header>
+        </div>
+    );
 }
 
 export default App;
