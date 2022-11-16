b0545a98dbabefd6e120ac2499eb4f2003afa120
Chiara Gandolfi <Chiara.Gandolfi@softwire.com>
2022-11-16 11:53:36 +0000

    tree component functional

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,19 @@ import './App.css';
 import NASA from "./components/NASA";
 import TitleParagraphImageComponent from "./components/TitleParagraphImageComponent";
 import ButtonWithCounter from "./components/ButtonWithCounter";
+import ComponentTree from "./components/ComponentTree";
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo"/>
-                <ButtonWithCounter></ButtonWithCounter>
-                <TitleParagraphImageComponent title={"Just a cat"} p1={"paragraph1"} p2={"paragraph2"}
-                                              image={"http://placekitten.com/200/300"}></TitleParagraphImageComponent>
+                {/*<ButtonWithCounter></ButtonWithCounter>*/}
+                {/*<TitleParagraphImageComponent title={"Just a cat"} p1={"paragraph1"} p2={"paragraph2"}*/}
+                {/*                              image={"http://placekitten.com/200/300"}></TitleParagraphImageComponent>*/}
 
-                <NASA></NASA>
+                {/*<NASA></NASA>*/}
+                <ComponentTree></ComponentTree>
             </header>
         </div>
     );
