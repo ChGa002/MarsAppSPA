1806d1c62521509641bfa24b833394c295ff4ebd
Chiara Gandolfi <Chiara.Gandolfi@softwire.com>
2022-11-16 11:16:57 +0000

    save button count value in local storage

--- src/components/ButtonWithCounter.js
+++ src/components/ButtonWithCounter.js
@@ -1,7 +1,20 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
+
+const counterKey = "counter";
 
 function ButtonWithCounter() {
     const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        if (count === 0)
+        {
+            setCount(parseInt(localStorage.getItem(counterKey)))
+        } else
+        {
+            localStorage.setItem(counterKey, count)
+        }
+    }, [count]);
+
     return (
         <>
             <p>Click counter: {count}</p>
